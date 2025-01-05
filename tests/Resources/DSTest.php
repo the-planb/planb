b5847f4b01f63d11deed39d082de4ab91f4c2a6c
jmpantoja <jmpantoja@gmail.com>
2025-01-05 21:02:55 +0100

    feat: Método Normalizer
    Se añade el método normalizer, que es similar a map, salvo que:
    - El callback solo recibe el value como parámetro (la key no se pasa)
    - Devuelve un objeto del mismo tipo
    
    La idea de esto es poder modificar todos los valores, sin cambiar de
    tipo. Por ejemplo, es útil para hacer algo como:
    TextList::normalize(strtolower(...))
    
    Tambien se permite usar este normalizer durante la creacción del objeto
    directamente desde el método collect

--- tests/Resources/DSTest.php
+++ tests/Resources/DSTest.php
@@ -13,7 +13,6 @@ use PlanB\Text\TextList;
 
 final class DSTest extends TestCase
 {
-
     public function test_it_map_alias_works()
     {
         $map = map();
@@ -46,21 +45,10 @@ final class DSTest extends TestCase
 
     public function test_it_text_collect_alias_works_when_has_a_callback()
     {
-        $text = textList(['a', 'b', 'c'], strtoupper(...));
+        $text = textList(['a', 'b', 'c'], fn(string $value) => strtoupper($value));
         $this->assertInstanceOf(TextList::class, $text);
 
         $this->assertEquals(['A', 'B', 'C'], $text->toArray());
     }
 
-    public function test_it_text_explode_alias_works()
-    {
-        $text = text_explode('');
-        $this->assertInstanceOf(TextList::class, $text);
-    }
-
-    public function test_it_text_split_alias_works()
-    {
-        $text = text_split('', '//');
-        $this->assertInstanceOf(TextList::class, $text);
-    }
 }
